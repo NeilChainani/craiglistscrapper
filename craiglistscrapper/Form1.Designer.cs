32db00c70c68590b278a1798daf3ec31bc9cbca3
NeilChainani <neilchainani1@gmail.com>
2015-03-10 12:30:05 -0400

    criaglist scrapper
    allows you to mass rip emails off of criaglist

---
+++ craiglistscrapper/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace craiglistscrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLink = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLink
+            // 
+            this.txtLink.Location = new System.Drawing.Point(44, 12);
+            this.txtLink.Name = "txtLink";
+            this.txtLink.Size = new System.Drawing.Size(264, 20);
+            this.txtLink.TabIndex = 1;
+            this.txtLink.Text = "http://harrisonburg.craigslist.org/search/pet";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Link";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-4, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Domain";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(135, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 101);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLink);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ craiglistscrapper/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace craiglistscrapper
+{
+    public partial class Form1 : Form
+    {
+       // private const string pattern = "<div class=\"anonemail\"> * </div>";
+      //  private const string pattern = "<div class=\"anonemail\"> * </div>";
+        //<div class="anonemail">td7jz-4876315202@pers.craigslist.org</div>
+        
+        //"(<div class=\"anonemail\" .*>)(.*)(<\\/div>)";
+
+        //[^<>]* any amount of characters
+        List<string> list = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+            // SimpleGET("test");
+
+          //  Extract("test");
+            getUrls(txtLink.Text);
+
+        }
+        public void getUrls(string url)
+        {//<p class="row"</p>
+           /* string pattern = "(<p[^<>]*class=\"row\"[^<>]*>)(.*)(<\\/p>)";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            Stream responseStream = ((HttpWebResponse)request.GetResponse()).GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string str = reader.ReadToEnd();
+            reader.Close();
+            responseStream.Flush();
+            responseStream.Close();*/
+            list.Clear();
+            var document = new HtmlWeb().Load(url);
+            HtmlNodeCollection rowNodes = document.DocumentNode.SelectNodes("//p[@class='row']/a[@href][1]");
+foreach (HtmlNode row in rowNodes)
+{
+
+    string link1 = row.Attributes["href"].Value;
+     link1 = link1.Replace(".html", "");
+   
+    string test = txtLink.Text.Substring(txtLink.Text.IndexOf("search"));//grabs search/pet
+    string test2 = txtLink.Text.Replace(test, "") + "reply" + link1; // gets rid of search/pet
+    var document2 = new HtmlWeb().Load(test2);
+    if (document2.DocumentNode.SelectSingleNode("//a[@href][@class='mailapp']") != null)
+    {
+        string meow = document2.DocumentNode.SelectSingleNode("//a[@href][@class='mailapp']").InnerText;
+        list.Add(meow);
+    }
+ 
+}
+//txtOutputBox.Text = node[0].InnerHtml;//.SelectNodes("//a")[0].OuterHtml;
+
+          //  txtOutputBox.Text = str;
+          /* // MatchCollection matches = Regex.Matches(str, pattern);
+            foreach (Match match in Regex.Matches(str, pattern))
+            {
+                string str3 = match.Groups[1].Value;
+              //  string test = "hello";
+            }*/
+
+
+        }
+        public void Extract(string index)
+        {
+            string pattern = "(<div[^<>]*class=\"anonemail\"[^<>]*>)(.*)(<\\/div>)";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(index);
+            request.Method = "GET";
+            Stream responseStream = ((HttpWebResponse)request.GetResponse()).GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            string str = reader.ReadToEnd();
+            reader.Close();
+            responseStream.Flush();
+            responseStream.Close();
+            MatchCollection matches = Regex.Matches(str, pattern);
+            if (matches.Count > 0)
+            {
+                foreach (Match m in matches)
+                {
+                    // m.Groups[0]
+                    //  Console.WriteLine("Inner DIV: {0}", );
+
+                  // txtOutputBox.Text =  m.Groups[2].Value;//1 gives u anonymousemail, 0 whole thing, 2 inside div
+
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt) | *.txt"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter writer = new StreamWriter(dialog.FileName))
+                {
+                    foreach (string str in list)
+                    {
+                        writer.WriteLine(str);
+                    }
+                }
+            }
+        }
+
+    }
+}
